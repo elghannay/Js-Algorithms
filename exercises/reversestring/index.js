d92e7f139c46f69960efa287979923e487abaf6e
Mohamed El Ghannay <mohamedelghannay@gmail.com>
2020-02-11 20:50:38 +0100

    added a solution for the reverse string method

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -1,11 +1,29 @@
-// --- Directions
-// Given a string, return a new string with the reversed
-// order of characters
-// --- Examples
-//   reverse('apple') === 'leppa'
-//   reverse('hello') === 'olleh'
-//   reverse('Greetings!') === '!sgniteerG'
+function reverse(str) {
+  return str
+    .split('')
+    .reverse()
+    .join('');
+}
 
-function reverse(str) {}
+console.log(reverse('hellow there'));
+function reverse(str) {
+  let reversed = '';
+  for (let character of str) {
+    // the trick is here the order in the concatenation is important
+    reversed = character + reversed;
+  }
+  return reversed;
+}
 
+function reverse(str) {
+  return str.split('').reduce(function(acc, current) {
+    acc = current + acc;
+    return acc;
+  }, '');
+}
+// ES6 solution
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
+reverse('hello there');
 module.exports = reverse;
