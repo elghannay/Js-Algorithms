659b4f32b765de575cafe30bcf592542a0801264
Elghannay Mohamed <mohamedelghannay@gmail.com>
2020-02-13 23:31:06 +0100

    anagram solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,20 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    let obj = {};
+    let max = 0;
+    let maxChar = '';
+    for (let char of string) {
+      obj[char] = ++obj[char] || 1; // using obj[char]++ is false in the loop you should use ++obj[char]
+    }
+    for (const key in obj) {
+      if (obj[key] > max) {
+        max = obj[key];
+        maxChar = key;
+      }
+    }
+    return maxChar;
+}
 
 module.exports = anagrams;
