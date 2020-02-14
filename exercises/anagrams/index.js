b7cc754d6a24a32d19f706a974d6a6952c930e92
Elghannay Mohamed <mohamedelghannay@gmail.com>
2020-02-14 01:23:37 +0100

    created two solution for the anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -7,7 +7,7 @@
 //   anagrams('rail safety', 'fairy tales') --> True
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
-
+// 'use strict';
 function anagrams(stringA, stringB) {
   const cleanedStrA = cleanStr(stringA);
   const cleanedStrB = cleanStr(stringB);
@@ -23,4 +23,27 @@ function cleanStr(str) {
     .join('');
 }
 
+// this one goes with it own helper function
+// since in this case we will create a solution
+// based on a object map and then check for each key on the first
+// is it equal the second
+function anagrams(stringA, stringB) {
+  const aCharMap = objectMap(stringA);
+  const bCharMap = objectMap(stringB);
+
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length)
+    return false;
+
+  for (let char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]) return false;
+  }
+}
+
+function objectMap(str) {
+  let obj = {};
+  for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+    obj[char] = obj[char] + 1 || 1;
+  }
+  return obj;
+}
 module.exports = anagrams;
