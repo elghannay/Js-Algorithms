03b7d4538db8c0f3ff15b518947d0468a6fb14b8
Elghannay Mohamed <mohamedelghannay@gmail.com>
2020-02-15 23:29:10 +0100

    create the capitalize algo

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,11 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let strToArray = str.split('');
+  for (const string of strToArray) {
+    string.slice(0).toUpperCase()
+}
+}
 
 module.exports = capitalize;
