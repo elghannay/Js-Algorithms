3cee3f418808a1d77ef84166468a29e8b6f6b965
elghannay <mohamedelghannay@gmail.com>
2020-03-19 23:41:45 +0100

    three new algorithms were completed

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,59 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+    if (n <= 1) return n;
+    return fib(n - 1) + fib(n - 2);
+}
+console.log(fibonacci(6));
 
-module.exports = fib;
+// linear runtime complexity since with every number we perform an additional 
+// iteration 
+function fibonacci(n) {
+    let second = 1;
+    let next;
+    let first = 0;
+    if (n <= 1) return n;
+    for (let i = 1; i < n; i++) {
+        next = first + second;
+        first = second;
+        second = next;
+    }
+    return next;
+}
+// another alternate solution 
+function fib(n) {
+    let result = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        result.push(result[i - 1] + result[i - 2])
+    }
+    return result[n];// return result[result.length -1]
+}
+// optimizing with memoization 
+
+function memoize(slowFn) {
+    let cache = {};
+    return function (...args) {
+        if (cache[args]) return cache[args];
+        let result = slowFn.apply(this, args);
+        cache[args] = result;
+        return result;
+    }
+}
+
+function slowFib(n) {
+    if (n <= 1) return n;
+    return fib(n - 1) + fib(n - 2);
+}
+let fib = memoize(slowFib)
+
+console.log(fibonacci(90));
+
+function fibonacci(n, previous = []) {
+    let result;
+    if (n <= 1) return n;
+    if (previous[n]) return previous[n];
+    result = fibonacci(n - 1, previous) + fibonacci(n - 2, previous);
+    previous[n] = result;
+    return result;
+}
--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -16,9 +16,41 @@
 //     [10,  9,  8, 7]]
 
 function matrix(n) {
-    // mot sure yet on how to solve this issue
-    // create an empty array 
-    // add to it each new array pushed as a new item 
-    // rely on the previous exercise
+    // set the array size
+    let result = [];
+    for (i = 0; i < n; i++) {
+        result.push([]);
+    }
+    let count = 1;
+    let rowStart = 0;
+    let rowEnd = n - 1;
+    let colStart = 0;
+    let colEnd = n - 1;
+    while (rowStart <= rowEnd && colStart <= colEnd) {
+        // filling the first row
+        for (let i = colStart; i <= colEnd; i++) {
+            result[rowStart][i] = count;
+            count++;
+        }
+        rowStart++;
+        //filling the last column
+        for (let i = rowStart; i <= rowEnd; i++) {
+            result[i][colEnd] = count;
+            count++;
+        }
+        colEnd--;
+        for (let i = colEnd; i >= colStart; i--) {
+            result[rowEnd][i] = count;
+            count++;
+        }
+        rowEnd--;
+        for (let i = rowEnd; i >= rowStart; i--) {
+            result[i][colStart] = count;
+            count++;
+        }
+        colStart++;
+    }
+    console.log(result);
 }
+matrix(8)
 module.exports = matrix;
