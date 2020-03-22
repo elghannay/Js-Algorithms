4702a8582b84f7f9ec2de5dfa0bca26f199a1467
elghannay <mohamedelghannay@gmail.com>
2020-03-22 04:02:07 +0100

    linked list continued

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -16,8 +16,71 @@ class LinkedList {
     insertFirst(data) {
         this.head = new Node(data, this.head);
     }
-}
+    size() {
+        let counter = 0;
+        let node = this.head;
+        while (node) {
+            counter++;
+            node = node.next;
+        }
+        return counter;
+    }
+    getFirst() {
+        return this.head;
+    }
+    getLast() {
+        if (!this.head)
+            return null;
+        let node = this.head;
+        while (node) {
+            if (!node.next)
+                return node;
+            node = node.next;
+        }
+    }
+    clear() {
+        this.head = null;
+    }
+    removeFirst() {
+        if (!this.head)
+            return null;
+        this.head = this.head.next;
+    }
+    removeLast() {
+        if (!this.head)
+            return null;
+        if (!this.head.next)
+            return this.head = null;
 
+        let previous = this.head;
+        let node = this.head.next;
+
+        while (node.next) {
+            previous = node;
+            node = node.next;
+        }
+        previous.next = null;
+    }
+    insertLast(data) {
+        if (!this.head)
+            this.head = new Node(data);
+        let last = this.getLast();
+        last.next = new Node(data);
+    }
+    getAt(index) {
+        let counter = 0;
+        let node = this.head;
+        while (node) {
+          if (counter === index) {
+            return node;
+          }
+    
+          counter++;
+          node = node.next;
+        }
+        return null;
+      }
+}
 // this is how to create a new linked list and associate 
 // a node to it.
 
@@ -26,5 +89,5 @@ class LinkedList {
     list.head = new Node(15);
 */
 
-class
-    module.exports = { Node, LinkedList };
+
+module.exports = { Node, LinkedList };
