bdebf36571bf2a88c90dc2cb5124b266c7ba229b
elghannay <mohamedelghannay@gmail.com>
2020-03-26 23:45:46 +0100

    selection sort and buble sort

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -5,15 +5,33 @@
 
 class Events {
   // Register an event handler
-  on(eventName, callback) {}
- // the idea here is to create an event and i dont know yet
+  constructor() {
+    this.events = {};
+  }
+  on(eventName, callback) {
+    this.events[eventName] ? this.events[eventName].push(callback) :
+      this.events[eventName] = [callback];
+    // if (this.events[eventName])
+    //   this.events[eventName].push(callback);
+    // else
+    //   this.events[eventName] = [callback];
+  }
+  // the idea here is to create an event and i dont know yet
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if (this.events[eventName]) {
+      for (const cb of this.events[eventName]) {
+        cb();
+      }
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    delete this.events[eventName];
+  }
 }
 
 module.exports = Events;
--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,12 +1,35 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
-
+let arr = [12, 52, 34, 1, 8, 0];
 function bubbleSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const lesser = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = lesser;
+            }
+        }
+    }
+    return arr;
 }
+console.log(selectionSort(arr));
+
 
 function selectionSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        let indexOfMin = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[indexOfMin])
+                indexOfMin = j;
+        }
+        if (arr[indexOfMin] < arr[i]) { // or if(indexOfMin !== i){}
+            let temp = arr[indexOfMin];
+            arr[indexOfMin] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    return arr;
 }
 
 function mergeSort(arr) {
